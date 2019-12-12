64910b180d320d8bea594023bbc8ced67c5427ca
Jonas <contact@atalantus.de>
2019-12-13 00:03:07 +0100

    Implemented three-dimensional grid generation

---
+++ Assets/Scripts/Level Generation/Grid.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+
+namespace LevelGeneration
+{
+    /// <summary>
+    /// Controls the grid
+    /// </summary>
+    public class Grid : MonoBehaviour
+    {
+        /// <summary>
+        /// Grid sizes
+        /// </summary>
+        public Vector3Int size;
+
+        /// <summary>
+        /// Grid cell scales
+        /// </summary>
+        public Vector3Int cellScale;
+
+        /// <summary>
+        /// Cell prefab
+        /// </summary>
+        public GameObject cellPrefab;
+
+        /// <summary>
+        /// Cells matrix ([width, height, depth])
+        /// </summary>
+        public Cell[,,] cells;
+
+        /// <summary>
+        /// <see cref="LevelGenerator"/>
+        /// </summary>
+        private LevelGenerator _levelGenerator;
+
+        /// <summary>
+        /// RNG seed
+        /// </summary>
+        public int seed;
+
+        private void Awake()
+        {
+            // TODO
+            //_levelGenerator = LevelGenerator.Instance;
+
+            GenerateGrid();
+
+            // Wave-function-collapse algorithm#
+            // TODO
+            //_levelGenerator.GenerateLevelWFC(ref cells, seed != -1 ? seed : Environment.TickCount);
+        }
+
+        /// <summary>
+        /// Generates the three-dimensional grid
+        /// </summary>
+        public void GenerateGrid()
+        {
+            if (size.x > 0 && size.y > 0 && size.z > 0)
+            {
+                // Generate grid
+                cells = new Cell[size.x, size.y, size.z];
+
+                var origin = transform.position;
+                var bottomLeft = new Vector3(
+                    origin.x - size.x * cellScale.x / 2f + cellScale.x / 2f,
+                    origin.y,
+                    origin.z - size.z * cellScale.z / 2f + cellScale.z / 2f
+                );
+                var curPos = bottomLeft;
+
+                for (int x = 0; x < size.x; x++)
+                for (int y = 0; y < size.y; y++)
+                for (int z = 0; z < size.z; z++)
+                {
+                    curPos = new Vector3(
+                        bottomLeft.x + x * cellScale.x,
+                        bottomLeft.y + y * cellScale.y,
+                        bottomLeft.z + z * cellScale.z
+                    );
+
+                    // Create new cell
+                    var cellObj = Instantiate(cellPrefab, curPos, Quaternion.identity, gameObject.transform);
+                    cellObj.name = $"({x}, {y}, {z})";
+                    var cell = cellObj.GetComponent<Cell>();
+                    cells[x, y, z] = cell;
+                }
+
+                Debug.Break();
+
+                // Assign neighbours for every cell
+                for (int x = 0; x < size.x; x++)
+                for (int y = 0; y < size.y; y++)
+                for (int z = 0; z < size.z; z++)
+                {
+                    var cell = cells[x, y, z];
+                    for (int i = 0; i < 6; i++)
+                    {
+                        int nx = x, ny = y, nz = z;
+
+                        switch (i)
+                        {
+                            case 0:
+                                nz++;
+                                break;
+                            case 1:
+                                nx++;
+                                break;
+                            case 2:
+                                nz--;
+                                break;
+                            case 3:
+                                nx--;
+                                break;
+                            case 4:
+                                ny++;
+                                break;
+                            case 5:
+                                ny--;
+                                break;
+                        }
+
+                        if (nx < 0 || ny < 0 || nz < 0 || nx > size.x - 1 || ny > size.y - 1 || nz > size.z - 1)
+                        {
+                            // Outside of grid`s dimensions
+                            // TODO
+                            //cell.neighbourCells[l] = null;
+                        }
+                        else
+                        {
+                            // TODO
+                            //cell.neighbourCells[l] = cells[x, z, k];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError("Impossible grid dimensions!", gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Destroys the current grid
+        /// </summary>
+        public void RemoveGrid()
+        {
+            foreach (Transform child in gameObject.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the grid is valid
+        /// </summary>
+        /// <returns>true if the grid is valid</returns>
+        public bool CheckGrid()
+        {
+            // TODO
+            //var notMatchingCells = _levelGenerator.CheckGeneratedLevel(ref cells);
+
+            //return notMatchingCells.Count == 0;
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Level Generation/LevelGenerator.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using Random = UnityEngine.Random;
+
+namespace LevelGeneration
+{
+    /// <summary>
+    /// Generates level using the wave-function-collapse algorithm
+    /// </summary>
+    public class LevelGenerator
+    {
+        private static LevelGenerator _instance;
+
+        /// <summary>
+        /// The Level Generator
+        /// </summary>
+        public static LevelGenerator Instance => _instance ?? (_instance = new LevelGenerator());
+
+        /// <summary>
+        /// Stores the cells in a heap having the closest cell to being solved as first element
+        /// </summary>
+        public Heap<Cell> OrderedCells;
+
+        private LevelGenerator()
+        {
+        }
+
+        /// <summary>
+        /// Wave-function-collapse algorithm
+        /// TODO: Could be multithreaded to increase performance
+        /// </summary>
+        /// <param name="cells">The grid`s cells</param>
+        /// <param name="seed">RNG seed</param>
+        public void GenerateLevelWFC(ref Cell[,] cells, int seed)
+        {
+            // Set RNG seed
+            Random.InitState(seed);
+
+            // Instantiate cells heap
+            OrderedCells = new Heap<Cell>(cells.GetLength(0) * cells.GetLength(1));
+
+            for (int i = 0; i < cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < cells.GetLength(1); j++)
+                {
+                    OrderedCells.Add(cells[i, j]);
+                }
+            }
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Debug.LogWarning("Start Wave-function-collapse algorithm");
+
+            // Make sure the level fits our initial constraints
+            ApplyInitialConstraints(ref cells);
+
+            // Wave-function-collapse Algorithm
+            while (true)
+            {
+                //Debug.Log("Starting another iteration! Removing next module.");
+
+                // Remove finished cells from heap
+                while (OrderedCells.Count > 0)
+                {
+                    var cell = OrderedCells.GetFirst();
+
+                    if (cell.SolvedScore == 1)
+                    {
+                        OrderedCells.RemoveFirst();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                // Remove random module from cell
+                if (OrderedCells.Count > 0)
+                {
+                    var cell = OrderedCells.GetFirst();
+                    cell.RemoveModule(cell.possibleModulesIndices[Random.Range(0, cell.possibleModulesIndices.Count)]);
+                }
+                else
+                {
+                    // Finished
+                    break;
+                }
+            }
+
+            stopwatch.Stop();
+            Debug.LogWarning(
+                $"Wave-function-collapse algorithm finished in {stopwatch.Elapsed.TotalMilliseconds}ms (Seed: {seed})");
+        }
+
+        /// <summary>
+        /// Checks if the cells of the generated level matches with each other
+        /// </summary>
+        /// <param name="cells">The grid`s cells</param>
+        /// <returns>List of not matching cells` (x, y)-coordinates</returns>
+        public List<Tuple<int, int>> CheckGeneratedLevel(ref Cell[,] cells)
+        {
+            var notMatchingCells = new List<Tuple<int, int>>();
+
+            for (int i = 0; i < cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < cells.GetLength(1); j++)
+                {
+                    var cell = cells[i, j];
+                    var bCell = cell.neighbourCells[0];
+                    var rCell = cell.neighbourCells[1];
+
+                    var matchesNeighbours = true;
+
+                    if (bCell != null)
+                    {
+                        if (ModuleManager.Instance.modules[cell.possibleModulesIndices[0]].edgeConnections[0] !=
+                            ModuleManager.Instance.modules[bCell.possibleModulesIndices[0]].edgeConnections[2])
+                        {
+                            matchesNeighbours = false;
+                            Debug.LogWarning($"CheckGeneratedLevel | ({i}, {j}) not matching with ({i}, {j + 1})");
+                        }
+                    }
+
+                    if (rCell != null)
+                    {
+                        if (ModuleManager.Instance.modules[cell.possibleModulesIndices[0]].edgeConnections[1] !=
+                            ModuleManager.Instance.modules[rCell.possibleModulesIndices[0]].edgeConnections[3])
+                        {
+                            matchesNeighbours = false;
+                            Debug.LogWarning($"CheckGeneratedLevel | ({i}, {j}) not matching with ({i + 1}, {j})");
+                        }
+                    }
+
+                    if (!matchesNeighbours) notMatchingCells.Add(new Tuple<int, int>(i, j));
+                }
+            }
+
+            return notMatchingCells;
+        }
+
+        /// <summary>
+        /// Resolve all initial constraints
+        /// </summary>
+        /// <param name="cells">The grid`s cells</param>
+        private void ApplyInitialConstraints(ref Cell[,] cells)
+        {
+            Debug.LogWarning("Resolve initial constraints");
+        }
+    }
+}
